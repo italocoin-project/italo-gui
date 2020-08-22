67327d5b7cb2118ae11811f84c4f434f7870e73f
ITALO <info@italo.network>
2020-08-22 10:00:58 +0200

    to master

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -17,7 +17,7 @@ if [ ! -d $MONERO_DIR/src ]; then
 fi
 git submodule update --remote
 git -C $MONERO_DIR fetch
-git -C $MONERO_DIR checkout v0.16.0.3
+git -C $MONERO_DIR checkout master
 
 # get italo core tag
 pushd $MONERO_DIR
